3ed6346214bf9a2bf050442f888cd36bb541e3a1
Rolson <jasonzrjc@gmail.com>
2017-07-28 17:04:09 +1000

    1

--- SIT_203/js/myFunctions.js
+++ SIT_203/js/myFunctions.js
@@ -3,51 +3,50 @@
  */
 
 // funtion to load the XML document
-function loadxml() {
-    try //Internet Explorer
-    {
-        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-        xmlDoc.async = false;
-        xmlDoc.load('product_details.xml');
-    }
-    catch (e) {
-        try //Firefox, Mozilla, Opera, etc.
-        {
-            xmlDoc = document.implementation.createDocument("", "", null);
-            xmlDoc.async = false;
-            xmlDoc.load('product_details.xml');
-        }
-        catch (e) {
-            try //Google Chrome
-            {
-                var xmlhttp = new window.XMLHttpRequest();
-                xmlhttp.open("GET", 'product_details.xml', false);
-                xmlhttp.send(null);
-                xmlDoc = xmlhttp.responseXML.documentElement;
-                console.log("successful");
-            }
-            catch (e) {
-                error = e.message;
-            }
-        }
-    }
-    return xmlDoc;
-}
+// function loadxml() {
+//     try //Internet Explorer
+//     {
+//         xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+//         xmlDoc.async = false;
+//         xmlDoc.load('product_details.xml');
+//     }
+//     catch (e) {
+//         try //Firefox, Mozilla, Opera, etc.
+//         {
+//             xmlDoc = document.implementation.createDocument("", "", null);
+//             xmlDoc.async = false;
+//             xmlDoc.load('product_details.xml');
+//         }
+//         catch (e) {
+//             try //Google Chrome
+//             {
+//                 var xmlhttp = new window.XMLHttpRequest();
+//                 xmlhttp.open("GET", 'product_details.xml', false);
+//                 xmlhttp.send(null);
+//                 xmlDoc = xmlhttp.responseXML.documentElement;
+//                 console.log("successful");
+//             }
+//             catch (e) {
+//                 error = e.message;
+//             }
+//         }
+//     }
+//     return xmlDoc;
+// }
 
 
 // function to test load
 function testLoad() {
-        var XMLFile = loadxml("../XML/Production_Details.xml");
+        // var XMLFile = loadxml("../XML/Production_Details.xml");
         $("#applyButton").click(function () {
-            var productionCase = $('<div></div>');
-            productionCase.append('<p>123312</p>');
+            var testDiv = $('<div></div>');
+            testDiv.append('<p>123312</p>');
 
-            $("#productionCase").html(productionCase);
+            $("#productionCase").html(testDiv);
             console.log("123123");
         })
 }
 
-
-$(document).ready(function () {
-    testLoad();
+$(document).ready(function(){
+   testLoad();
 });
